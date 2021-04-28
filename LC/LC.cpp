e22c238833730811f48ac3efa1e2d55d9e80294f
Cailgun <314839748@qq.com>
2021-04-28 16:18:55 +0800

    完成题目1011

--- LC/LC.cpp
+++ LC/LC.cpp
@@ -4,9 +4,85 @@
 #include <functional>
 using namespace std;
 
+int load(std::vector<int>& weights, int capacity)
+{
+	int count = 0;
+	int day = 1;
+	for (auto iter : weights)
+	{
+		if (count + iter > capacity)
+		{
+			count = 0;
+			day++;
+		}
+		count += iter;
+	}
+	return day;
+}
+
+int findCapacity(int left, int right, std::vector<int>& weights, int D)
+{
+	if (left == right)
+	{
+		return left;
+	}
+	if (abs(left - right) == 1)
+	{
+		return (load(weights, left) == D) ? left : right;
+	}
+
+	int mid = (left + right) / 2;
+	int currentD = load(weights, mid);
+	if (currentD > D)
+	{
+		return findCapacity(mid, right, weights, D);
+	}
+	else if (currentD < D)
+	{
+		return findCapacity(left, mid, weights, D);
+	}
+
+	int lastD = load(weights, mid - 1);
+
+	if (lastD == D)
+	{
+		return findCapacity(left, mid, weights, D);
+	}
+	else
+	{
+		return mid;
+	}
+}
+
+int shipWithinDays(std::vector<int>& weights, int D) {
+	size_t size = weights.size();
+	if (size == 0)
+	{
+		return 0;
+	}
+	int capacity = 1;
+	for (auto iter : weights)
+	{
+		if (iter > capacity)
+			capacity = iter;
+	}
+	int lastCapacity = capacity;
+	while (true)
+	{
+		int currentD = load(weights, capacity);
+		if (currentD <= D)
+		{
+			return findCapacity(lastCapacity, capacity, weights, D);
+		}
+		lastCapacity = capacity;
+		capacity *= 2;
+	}
+}
+
 int main()
 {
-
+	vector<int> v({10, 50, 100, 100, 50, 100, 100, 100});
+	int i = shipWithinDays(v, 5);
 
 	return 0;
 }
--- LC/_1011_在D天内送达包裹的能力.h
+++ LC/_1011_在D天内送达包裹的能力.h
@@ -4,8 +4,79 @@ class _1011_在D天内送达包裹的能力
 {
 	class Solution {
 	public:
-		int shipWithinDays(std::vector<int>& weights, int D) {
+		int load(std::vector<int>& weights, int capacity)
+		{
+			int count = 0;
+			int day = 1;
+			for (auto iter : weights)
+			{
+				if (count + iter > capacity)
+				{
+					count = 0;
+					day++;
+				}
+				count += iter;
+			}
+			return day;
+		}
 
+		int findCapacity(int left, int right, std::vector<int>& weights, int D)
+		{
+			if (left == right)
+			{
+				return left;
+			}
+			if (abs(left - right) == 1)
+			{
+				return (load(weights, left) == D) ? left : right;
+			}
+
+			int mid = (left + right) / 2;
+			int currentD = load(weights, mid);
+			if (currentD > D)
+			{
+				return findCapacity(mid, right, weights, D);
+			}
+			else if (currentD < D)
+			{
+				return findCapacity(left, mid, weights, D);
+			}
+
+			int lastD = load(weights, mid - 1);
+
+			if (lastD == D)
+			{
+				return findCapacity(left, mid, weights, D);
+			}
+			else
+			{
+				return mid;
+			}
+		}
+
+		int shipWithinDays(std::vector<int>& weights, int D) {
+			size_t size = weights.size();
+			if (size == 0)
+			{
+				return 0;
+			}
+			int capacity = 1;
+			for (auto iter : weights)
+			{
+				if (iter > capacity)
+					capacity = iter;
+			}
+			int lastCapacity = capacity;
+			while (true)
+			{
+				int currentD = load(weights, capacity);
+				if (currentD <= D)
+				{
+					return findCapacity(lastCapacity, capacity, weights, D);
+				}
+				lastCapacity = capacity;
+				capacity *= 2;
+			}
 		}
 	};
 };
